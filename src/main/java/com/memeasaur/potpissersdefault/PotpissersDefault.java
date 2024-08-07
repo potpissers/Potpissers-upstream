a24b24c3e1058620241afc266b676612e5ecba2e
Cameron <cuckmeron@gmail.com>
2024-08-07 19:53:11 -0400

    init

---
+++ src/main/java/com/memeasaur/potpissersdefault/PotpissersDefault.java
@@ -0,0 +1,12 @@
+package com.memeasaur.potpissersdefault;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PotpissersDefault extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
